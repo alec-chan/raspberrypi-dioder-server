4d23edd9e6d962d1eccc84e73aa4cfff4e764540
Alec Chan <alecchan1@gmail.com>
2017-06-18 17:36:34 -0700

    init commit

---
+++ Public/script.js
@@ -0,0 +1,39 @@
+var color='#ffffff';
+var off=true;
+
+var colorcontrol = document.getElementById("color");
+var offcontrol = document.getElementById("off");
+
+function requestSettings(){
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange=function(){
+        if(xhr.readyState===XMLHttpRequest.DONE){
+            if(xhr.status===200){
+                var status = JSON.parse(xhr.responseText);
+                setSettings(status);
+            }
+        }
+    }
+}
+
+function setSettings(resp){
+    colorcontrol.jscolor.fromString(resp.color);
+    offcontrol.checked=resp.off;
+}
+
+
+function update(jscolor) {
+    color='#' + jscolor;
+}
+
+function toggleOff(cb){
+    off=cb.checked;
+}
+
+
+function submit(){
+    var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+    xmlhttp.open("POST", "/submit");
+    xmlhttp.setRequestHeader("Content-Type", "application/json");
+    xmlhttp.send(JSON.stringify({off:off, color: color}));
+}
---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var Redoid = require('redoid');
+var redoid = Redoid({
+    color: '#ffffff'
+});
+var off=false;
+
+app.use( bodyParser.json() );
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/Public/index.html'));
+});
+
+app.get('/status', function(req, res){
+    res.setHeader('Content-Type', 'application/json');
+    var color=redoid.getColor();
+    res.send(JSON.stringify({ off: off, color: color}));
+});
+
+app.post('/submit', function(req, res) {
+    var name = req.body.name,
+        color = req.body.color;
+    if(req.body.off){
+        redoid.turnOff();
+        off=true;
+    }
+    else
+    {
+        if(req.body.color){
+            if(redoid.isColorValid(req.body.color)){
+                redoid.transition(req.body.color, 1500);
+                off=false;
+            }
+        }
+    }
+});
+
+app.listen(8080);
