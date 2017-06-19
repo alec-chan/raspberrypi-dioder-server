ae51e58580d5b23fdcb1798a6b50c0d1d24e4b21
Alec Chan <alecchan1@gmail.com>
2017-06-18 17:50:05 -0700

    add logging.

--- Public/script.js
+++ Public/script.js
@@ -9,12 +9,14 @@ window.onload=function(){
 };
 
 function requestSettings(){
+    console.log('requesting settings');
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange=function(){
         if(xhr.readyState===XMLHttpRequest.DONE){
             if(xhr.status===200){
                 var status = JSON.parse(xhr.responseText);
                 setSettings(status);
+                console.log('got settings, '+status);
             }
         }
     }
